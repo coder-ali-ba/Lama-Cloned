2d3d243ee97f4c72c8d24f59ee7b6072cd73d246
coder-ali-ba <habibali8770@gmail.com>
2025-12-07 21:06:14 +0500

    single Product

---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+function ProductImages() {
+  return (
+    <div>
+      <div className='h-[500px] relative'>
+        <Image src="" alt=''  sizes='50vw' fill className='object-cover rounded-md'/>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default ProductImages
