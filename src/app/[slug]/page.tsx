cb5802060020526f0181914a29f63f3fd4170674
coder-ali-ba <habibali8770@gmail.com>
2025-12-17 23:02:42 +0500

    Customize

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Add from '@/components/Add'
+import CustomizeProduct from '@/components/CustomizeProduct'
 import ProductImages from '@/components/ProductImages'
 import React from 'react'
 
@@ -15,9 +17,25 @@ function SinglePage() {
         <div className='h-[2px] bg-gray-100'/>
         <div className='flex items-center gap-4'>
           <h3 className='text-xl text-gray-500 line-through'>$50</h3>
-          <h2>$40</h2>
+          <h2 className='font-medium text-2xl'>$40</h2>
         </div>
         <div className='h-[2px] bg-gray-100'/>
+        <CustomizeProduct />
+        <div className='h-[2px] bg-gray-100'/>
+        <Add />
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat reiciendis doloribus iusto debitis, neque, at ex rerum modi quam molestiae praesentium. Dolorem, minus atque?</p>
+        </div>
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat reiciendis doloribus iusto debitis, neque, at ex rerum modi quam molestiae praesentium. Dolorem, minus atque?</p>
+        </div>
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat reiciendis doloribus iusto debitis, neque, at ex rerum modi quam molestiae praesentium. Dolorem, minus atque?</p>
+        </div>
+        
       </div>
     </div>
   )
--- src/components/CustomizeProduct.tsx
+++ src/components/CustomizeProduct.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function CustomizeProduct() {
   return (
-    <div>
-      Customize products
+    <div className='flex flex-col gap-6'>
+      <h4 className='font-medium'>Choose a color</h4>
+      <div className='flex'></div>
+      <h4 className='font-medium'>Choose a size</h4>
+      <div></div>
     </div>
   )
 }
